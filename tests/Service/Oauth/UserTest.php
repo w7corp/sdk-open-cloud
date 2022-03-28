0425e2b7bbd54db41e601bdbee0da633a9202aef
titrxw <1120309488@qq.com>
2022-03-28 11:27:13 +0800

    添加service request

--- tests/Service/Oauth/UserTest.php
+++ tests/Service/Oauth/UserTest.php
@@ -3,13 +3,10 @@
 namespace W7\Sdk\OpenCloud\Tests\Service\Oauth;
 
 use PHPUnit\Framework\TestCase;
-use W7\Sdk\OpenCloud\Request\Request;
 use W7\Sdk\OpenCloud\Service\Oauth\User;
 
 class UserTest extends TestCase {
 	public function testGetUserInfoByJsCode() {
-		Request::setEnvBeta();
-
 		$result = User::instance()
 			->withAppId('test')
 			->withAppSecret('d659404b91c7e5546fe')
